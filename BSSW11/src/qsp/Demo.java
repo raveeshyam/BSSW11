0cc59f400801f72236d405fc9425460c5f8a565d
javagalbhanu <javagalbhanu@gmail.com>
2017-03-03 13:44:00 +0530

    Hi

--- BSSW11/src/qsp/Demo.java
+++ BSSW11/src/qsp/Demo.java
@@ -1,5 +1,11 @@
 package qsp;
 
-public class Demo {
+import org.testng.Reporter;
+import org.testng.annotations.Test;
 
+public class Demo {
+	@Test
+	public void testA(){
+		Reporter.log("Hi",true);
+	}
 }
